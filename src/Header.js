1d1bab4710e9a08afaddd277bc525d60b3d19b02
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-04-12 13:18:26 -0700

    end of am lecture

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // a React component is a function that returns some HTML to be displayed on the page
-function Header() {
+function Header({ numberOfModules, numberOfClicks, incrementNumberOfClicks }) {
   let schoolName = 'Thinkful';
+  // wishful thinking... we have to use state variables for anything that gets updated
+  // let numberOfClicks = 0;
+  //    variable          setter for variable        initial value
+  
   return (
-    <React.Fragment>
-      <h1 className="school-name">{schoolName}</h1>
-      <h2>The coolest school</h2>
-    </React.Fragment>
+    <header>
+      <h1 className="school-name" onClick={incrementNumberOfClicks}>{schoolName}</h1>
+      <h2>The coolest school. You have clicked {numberOfClicks} times.</h2>
+      <h2>There are {numberOfModules} modules.</h2>
+    </header>
   )
 
 }
